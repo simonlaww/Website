57d3e30c9e9a847d6a5dc640f720d81c870c8368
Simon Law <simonlaw2468@gmail.com>
2017-05-14 22:49:36 -0700

    Added .when to module routing

--- public/js/routes.js
+++ public/js/routes.js
@@ -1,7 +1,6 @@
 var test = angular.module('test', ['ngRoute']).
 	config(['$routeProvider', function($routeProvider) {
 		$routeProvider.
-			// when('/about', {template:'partials/about.html'}).
-			// when('/experiments', {template:'partials/experiments.html'}).
-			otherwise({redirectTo:'/', template:'../views/index.html'});
+     		when('/', {template:'../views/index.html'}).
+     		otherwise({redirectTo:'/'});
 	}]);
